11ddc9d6a8f75ef8f307f90e4b057512cb0e3801
FranciscoMaik <chiconunes3081@gmail.com>
2022-09-10 20:49:07 -0300

    feat: configation project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Testo</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
